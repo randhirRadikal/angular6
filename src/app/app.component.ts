5c7a88c078761e9f5702463c773d177e265b1c35
Randhir Jha <randhir@radikal-labs.com>
2018-09-14 16:22:29 +0530

    admin panal

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+﻿import { Component,OnInit } from '@angular/core';
+
+
+@Component({
+    selector: 'app',
+    templateUrl: 'app.component.html'
+})
+export class AppComponent implements OnInit {
+    isUserLogin = false;
+    submitted = false;
+
+    constructor(
+    ) {
+
+    }
+
+    ngOnInit() {
+
+    }
+
+    ngAfterViewChecked(){
+        if (localStorage.getItem('currentUser')) {
+          this.isUserLogin = true;
+        }else{
+          this.isUserLogin = false;
+        }
+    }
+
+    onSubmit() {
+
+    }
+}
---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { BlankComponent } from './blank';
+import { AuthGuard } from './_guards';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'blank', component: BlankComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: 'blank' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
